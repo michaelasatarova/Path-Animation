bb95b63b925c3a2bcee7fd1e09e0f7f424e73705
Michaela Šatarová <michaela.satarova.ml@gmail.com>
2021-08-24 21:42:49 +0200

    js

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
-var path = document.querySelector('.path');
-var length = path.getTotalLength();
 
-console.log("Total length of this path:", length, "= 100%");
+var progress = document.getElementById('wave2');
+var pathLength = progress.getTotalLength();
 
+progress.style.strokeDasharray = pathLength + 'px';
+progress.style.strokeDashoffset = pathLength + 'px';
+ 
+
+function test (arg){ 
+    // change number from px to % (trojčlenka)
+        let percentage = (arg * pathLength ) / 100 ; 
+    // set up new percentage
+        let newOffset = parseInt( progress.style.strokeDashoffset ) - percentage;
+    // change new percentage in DOM 
+        progress.style.strokeDashoffset = newOffset;
+        console.log(percentage);
+}
+test(50);
